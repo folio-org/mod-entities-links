c28c82a41787af4c278b5a7c6b8b544a102494ec
psmagin <pavlo_smahin@epam.com>
2026-01-20 16:50:01 +0200

    test2

--- src/main/java/org/folio/entlinks/service/tenant/ExtendedTenantService.java
+++ src/main/java/org/folio/entlinks/service/tenant/ExtendedTenantService.java
@@ -2,6 +2,7 @@ package org.folio.entlinks.service.tenant;
 
 import lombok.extern.log4j.Log4j2;
 import org.folio.entlinks.service.dataloader.ReferenceDataLoader;
+import org.folio.entlinks.service.settings.TempSettingsMigrationService;
 import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.liquibase.FolioSpringLiquibase;
 import org.folio.spring.service.PrepareSystemUserService;
